5aaef013e1a5187916280c5abc13eb3570e5e77a
Bingxuan Ying <yingbingxuan@gmail.com>
2019-12-29 23:04:03 -0800

    Add basic homepage layout

--- website/src/App.js
+++ website/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import HomePage from "./component/HomePage";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
+        <HomePage />
       </div>
     );
   }
