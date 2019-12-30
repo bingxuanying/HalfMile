c24b864fae36a952915ce8cb88c02637059bc1c5
CharlesYWL <xlyin@ucdavis.edu>
2019-12-30 01:23:36 -0800

    A

--- website/src/component/Footer/FooterContainer.js
+++ website/src/component/Footer/FooterContainer.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./FooterContainer.css";
+import FooterButtom from "./FooterButtom/FooterButtom";
 
 class FooterContainer extends Component {
   render() {
     return (
       <div className="footer-container">
         <div className="footer-container-content"></div>
-        <div className="footer-container-footer"></div>
+        <div className="footer-container-footer">
+          <FooterButtom />
+        </div>
       </div>
     );
   }
