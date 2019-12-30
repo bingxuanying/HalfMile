91e10bce6d4cc0102b87f479a1182d96e96478bb
CharlesYWL <34050457+CharlesYWL@users.noreply.github.com>
2019-12-30 04:16:20 -0800

    add CardHolder

---
+++ website/src/component/Card/Card.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import "./Card.sass";
+import image from "./tokyo.jpg";
+import { Card, Button } from 'react-bootstrap';
+
+class CardHolder extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            width: '18rem',
+            buttonTitle: "See Detail",
+            cardTitle: "Tokyo 3 Days",
+            cardSubTitle: "Weili",
+            cardText: "Nice Trip to Tokyo"
+        };
+    }
+    render() {
+        return (
+            <Card className="card" style={{ width: this.state.width }}>
+                <Card.Img variant="top" src={image} />
+                <Card.Body>
+                    <Card.Title>{this.state.cardTitle}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{this.state.cardSubTitle}</Card.Subtitle>
+                    <Card.Text>{this.state.cardText}</Card.Text>
+                    <Button href="#">{this.state.buttonTitle}</Button>
+                </Card.Body>
+            </Card>
+        );
+    }
+}
+
+export default CardHolder;
--- website/src/component/HomePage.js
+++ website/src/component/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import HeaderContainer from "./Header/HeaderContainer";
 import FooterContainer from "./Footer/FooterContainer";
+import Card from "./Card/Card";
 import "./HomePage.css";
 
 // For TEST Purpose
@@ -8,6 +9,9 @@ class HomePage extends Component {
   render() {
     return (
       <div className="homepage">
+        <div className="hompage-recomendation">
+          <Card />
+        </div>
         <footer className="homepage-footer">
           <FooterContainer />
         </footer>
