cbf8ad61a3dd597356da08f98c4668589fd2374e
Jerryhe <lolhe@ucdavis.edu>
2019-12-31 00:42:04 -0800

    add buttons

--- website/src/component/Header/HeaderNav/HeaderNav.js
+++ website/src/component/Header/HeaderNav/HeaderNav.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import HeaderLogo from "../HeaderLogo/HeaderLogo";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button, Navbar, Nav, NavDropdown, Form, FormControl } from 'react-bootstrap';
+import { Button, Navbar, Nav, DropdownButton, NavItem, MenuItem} from 'react-bootstrap';
+import { icon } from "@fortawesome/fontawesome-svg-core";
 // import AddProductModal from "../UserLogin/AddProductModal";
 
 class HeaderNav extends Component {
@@ -11,18 +12,11 @@ class HeaderNav extends Component {
         <Navbar.Brand className="NavLogo"><HeaderLogo />AnyWhere</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="BarInfo">
-          <Nav className="mr-auto">
-            <NavDropdown title="Travel Everywhere">
-              <NavDropdown.Item href="#Plan" id="Plan">Your Plan</NavDropdown.Item>
-              <NavDropdown.Item href="#Hotel" id="Hotel">Your Hotel</NavDropdown.Item>
-              <NavDropdown.Item href="#Flight" id="Flight">Your Flight</NavDropdown.Item>
-            </NavDropdown>
-            
-          </Nav>
+          <Nav className="mr-auto" />           
           <Nav>
-            <Nav.Link href="#Home">Home</Nav.Link>           
-            <Nav.Link href="#Register">Register</Nav.Link>
-            <Nav.Link href="#Login">Login</Nav.Link>
+            <Nav.Link href="#Home"><Button id="home-buttom" size="lg" variant="outline-dark">Home</Button></Nav.Link>           
+            <Nav.Link href="#Register"><Button size="lg" variant="outline-dark">Register</Button></Nav.Link>
+            <Nav.Link href="#Login"><Button size="lg" variant="outline-dark">Login</Button></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
