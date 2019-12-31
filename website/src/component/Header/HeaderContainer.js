3661b801fc3d5efabf27e758a43da94f350f4344
Jerryhe <lolhe@ucdavis.edu>
2019-12-30 20:56:51 -0800

    Add Header logo, Add NavBar
    Add Header logo, Add NavBar

--- website/src/component/Header/HeaderContainer.js
+++ website/src/component/Header/HeaderContainer.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+import HeaderNavbar from "./HeaderNav/HeaderNav";
+import HeaderLogo from "./HeaderLogo/HeaderLogo";
+import "./Header.css";
 
 class HeaderContainer extends Component {
   render() {
-    return <div></div>;
+    return(
+      <HeaderNavbar />
+    );
   }
 }
 
