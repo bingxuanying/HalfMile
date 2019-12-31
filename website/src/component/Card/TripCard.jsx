3eb00cef28a48d2e46eb22b277516e4eb21f66e3
CharlesYWL <34050457+CharlesYWL@users.noreply.github.com>
2019-12-31 04:56:58 -0800

    add vw to tripCard

--- website/src/component/Card/TripCard.jsx
+++ website/src/component/Card/TripCard.jsx
@@ -1,26 +1,32 @@
 import React, { Component } from "react";
 import image from "./assets/mytrip.png";
-import { Card } from "react-bootstrap";
-import { MdStar, MdStarHalf } from 'react-icons/md';
+import { Card, Button } from "react-bootstrap";
 import "./index.sass";
 
 class TripCard extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            tripTitle: "My Trip",
+            tripDuration: 5,
+            startData: new Date()
         };
     }
     render() {
+        console.log(this.state.startData);
         return (
-            <Card className="trip-card">
+            <Card className="trip-card card border-0 text-left ml-0 mr-3">
                 <Card.Img className="trip-card-img" variant="top" src={image} />
-                {/* <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the bulk of
-                        the card's content.
-                    </Card.Text>
-                </Card.Body> */}
+                <div className="trip-card-shade">
+                    <div className="trip-card-days">
+                        5days
+                    </div>
+                    {/* trip-card-title includes startData and title */}
+                    <div className="trip-card-title">
+                        my fking trip
+                    </div>
+                    <div className="trip-card-option"><Button /></div>
+                </div>
             </Card>
         );
     }
