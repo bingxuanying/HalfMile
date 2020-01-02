eac1c1200f30cd291e8b5a47ae26c8f3f1dcc8dd
Jerryhe <lolhe@ucdavis.edu>
2020-01-01 23:10:17 -0800

    Updated NavBar

--- website/src/component/Header/HeaderNav/HeaderNav.js
+++ website/src/component/Header/HeaderNav/HeaderNav.js
@@ -1,16 +1,9 @@
 import React, { Component } from "react";
 import HeaderLogo from "../HeaderLogo/HeaderLogo";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  Button,
-  Navbar,
-  Nav,
-  DropdownButton,
-  NavItem,
-  MenuItem
-} from "react-bootstrap";
+import "./HeaderNav.css";
+import { Navbar, Nav} from "react-bootstrap";
 import Login from "../User/Login";
-// import AddProductModal from "../UserLogin/AddProductModal";
 
 class HeaderNav extends Component {
   constructor() {
@@ -37,34 +30,34 @@ class HeaderNav extends Component {
     const { isTop } = this.state;
 
     return (
-      <Navbar fixed="top" bg={!isTop && "white"} expand="lg">
+      <Navbar id="Bar" fixed="top" bg={!isTop && "white"} expand="lg">
         <Navbar.Brand className="NavLogo">
           <HeaderLogo />
           AnyWhere
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="BarInfo">
+        <Navbar.Collapse id="navbar">
           <Nav className="mr-auto" />
           <Nav>
             <Nav.Link href="#Home">
-              <Button id="home-buttom" size="lg" variant="outline-dark">
+              <a id="Help-buttom" size="lg" variant="outline-dark">
                 Home
-              </Button>
+              </a>
             </Nav.Link>
             <Nav.Link href="#Help">
-              <Button id="Help-buttom" size="lg" variant="outline-dark">
+              <a id="Help-buttom" size="lg" variant="outline-dark">
                 Help
-              </Button>
+              </a>
             </Nav.Link>
             <Nav.Link href="#Register">
-              <Button size="lg" variant="outline-dark">
+              <a id="Help-buttom" size="lg" variant="outline-dark">
                 Register
-              </Button>
+              </a>
             </Nav.Link>
             <Nav.Link href="#Login">
-              <Button size="lg" variant="outline-dark">
+              <a id="Help-buttom" size="lg" variant="outline-dark">
                 Login
-              </Button>
+              </a>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
