29764da49fd01c9ce0baa96a5c6ea8918c15d2a2
CharlesYWL <34050457+CharlesYWL@users.noreply.github.com>
2020-01-01 23:50:00 -0800

    add input handler

--- website/src/component/SearchBar/SearchBar.jsx
+++ website/src/component/SearchBar/SearchBar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Form, FormGroup } from "react-bootstrap";
 import { FaSearch } from "react-icons/fa";
 import "./index.sass";
+import { ControlLabel } from "react-bootstrap";
 
 class SearchBar extends Component {
   constructor(props) {
@@ -16,10 +17,23 @@ class SearchBar extends Component {
       <Form className="search-bar">
         <Form.Group controlId="keyword">
           <FaSearch className="search-bar-icon" />
-          <Form.Control className="search-bar-contenent" type="text" placeholder="Enter Destenation" />
+          <Form.Control className="search-bar-contenent" type="text"
+            placeholder="Enter Destenation" onChange={this.handleChange}
+            onKeyPress={this.handleKeyPress} />
         </Form.Group>
       </Form>
     );
+  }
+
+  handleChange = (event) => {
+    this.setState({ input: event.target.value })
+    // console.log(event.target.value);
+  }
+  handleKeyPress = (event) => {
+    var code = event.keyCode || event.which;
+    if (code === 13) { //13 is the enter keycode
+      console.log("We enter ENTER");
+    }
   }
 }
 
