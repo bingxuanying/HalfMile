e71729c403a499d88398681964778adc796b2eeb
CharlesYWL <34050457+CharlesYWL@users.noreply.github.com>
2020-01-06 20:27:52 -0800

    finish TripOverview Day

--- website/src/component/TripOverview/TripOverviewDay/TripOverviewDay.jsx
+++ website/src/component/TripOverview/TripOverviewDay/TripOverviewDay.jsx
@@ -6,7 +6,8 @@ import HotelIcon from "@material-ui/icons/Hotel";
 import FlightTakeoffIcon from "@material-ui/icons/FlightTakeoff";
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
-import { Tooltip } from "@material-ui/core";
+import RoomIcon from '@material-ui/icons/Room';
+import { Tooltip, Fade } from "@material-ui/core";
 
 import "./index.sass";
 class TripOverviewDay extends Component {
@@ -20,11 +21,12 @@ class TripOverviewDay extends Component {
       isEnd: this.props.isEnd,
       startCity: this.props.startCity,
       endCity: this.props.endCity,
-      hotel: ["Marroit JW SanFranscico", "HalfMoon Bay Riz-Carlton"],
-      transport: ["UA8848"],
-      activities: [{ "name": "Union Square", "cost": 165, "time": "1h" },
-      { "name": "Golden Gate", "cost": 0, "time": "2h" },
-      { "name": "Fisherman's Wharf", "cost": 100, "time": "1.5h" }]
+      hotel: [{ "name": "Marroit JW SanFranscico", "price": 182, "startDate": "2/1/2020", "endDate": "2/3/2020" },
+      { "name": "SanFransciso IHG", "price": 555, "startDate": "2/3/2020", "endDate": "2/5/2020" }],
+      transport: [{ "flight": "UA8848", "departDate": "2/1/2020", "arriveDate": "2/1/2020", "departTime": "12:00", "arriveTime": "14:00", "price": 1024 }],
+      activities: [{ "name": "Union Square", "price": 165, "time": "1h" },
+      { "name": "Golden Gate", "price": 0, "time": "2h" },
+      { "name": "Fisherman's Wharf", "price": 100, "time": "1.5h" }]
     };
   }
 
@@ -87,17 +89,18 @@ class TripOverviewDay extends Component {
   }
   getInfo = () => {
     return (
-      <div className="trip-overview-day-text d-inline-block">
-        <div className="hotel mb-1">
+      <div className="trip-overview-day-text text-truncate">
+        <div className="hotel mb-0">
           <HotelIcon />
-          <Tooltip title={this.MoreInfo()}>
+          <Tooltip TransitionComponent={Fade} TransitionProps={{ timeout: 600 }}
+            title={this.MoreInfo()}>
             <InfoOutlinedIcon style={{ marginLeft: "42px" }} fontSize="small" />
           </Tooltip>
           {this.state.hotel.map(function (hotel, index) {
             return (
-              <p className="hotel-name mb-0 pl-0" key={index}>
+              <p className="hotel-name mb-0 pl-0 " key={index}>
                 <FiberManualRecordIcon style={{ fontSize: "8" }} />
-                {hotel}
+                {hotel.name}
               </p>
             );
           })}
@@ -108,7 +111,7 @@ class TripOverviewDay extends Component {
             return (
               <p className="transport-name mb-0 pl-0" key={index}>
                 <FiberManualRecordIcon style={{ fontSize: "8" }} />
-                {trans}
+                {trans.flight}
               </p>
             );
           })}
@@ -141,26 +144,63 @@ class TripOverviewDay extends Component {
   MoreInfo = () => {
     // Almost same as getInfo()
     return (
-      <div className="trip-overview-day-text d-inline-block">
-        <div className="hotel mb-1">
+      <div className="more-info-text p-1">
+        <div className="hotel mb-2">
           <HotelIcon />
           {this.state.hotel.map(function (hotel, index) {
             return (
-              <p className="hotel-name mb-0 pl-0" key={index}>
-                <FiberManualRecordIcon style={{ fontSize: "8" }} />
-                {hotel}
-              </p>
+              <div className="hotel-content mb-1 pl-1" key={index}>
+                <p className="hotel-text mb-1 pl-0" >
+                  <span className="hotel-name">
+                    <FiberManualRecordIcon style={{ fontSize: "8" }} />
+                    {hotel.name}
+                  </span>
+                  <span className="hotel-price">${hotel.price}</span>
+                </p>
+                <p className="hotel-date mb-2">
+                  &nbsp;{hotel.startDate}&nbsp;~&nbsp;{hotel.endDate}
+                </p>
+              </div>
             );
           })}
         </div>
-        <div className="transport mb-1">
+        <div className="transport mb-2">
           <FlightTakeoffIcon />
           {this.state.transport.map(function (trans, index) {
             return (
-              <p className="transport-name mb-0 pl-0" key={index}>
-                <FiberManualRecordIcon style={{ fontSize: "8" }} />
-                {trans}
-              </p>
+              <div className="transport-content mb-1 pl-1">
+                <p className="transport-text mb-1 pl-0" key={index}>
+                  <span className="transport-name">
+                    <FiberManualRecordIcon style={{ fontSize: "8" }} />
+                    {trans.flight}
+                  </span>
+                  <span className="transport-price">${trans.price}</span>
+                </p>
+                <p className="transport-date mb-2">
+                  &nbsp;{trans.departDate}&nbsp;{trans.departTime}&nbsp;~&nbsp;{trans.arriveTime}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+        <div className="activity mb-1">
+          <RoomIcon />
+          {this.state.activities.map(function (activity, index) {
+            return (
+              <div className="activity-content">
+                <p className="activity-text mb-1 pl-0" key={index}>
+                  <span className="activity-name">
+                    <FiberManualRecordIcon style={{ fontSize: "8" }} />
+                    {activity.name}
+                  </span>
+                  <span className="activity-price">
+                    ${activity.price}
+                  </span>
+                </p>
+                <p className="activity-info mb-2">
+                  &nbsp;Time:{activity.time}
+                </p>
+              </div>
             );
           })}
         </div>
