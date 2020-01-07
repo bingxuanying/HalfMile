b625994d4a673208fc3eb369e5fd24ba2975858b
Jerryhe <lolhe@ucdavis.edu>
2020-01-06 22:47:28 -0800

    Update HeaderLogo.js

--- website/src/component/Header/HeaderLogo/HeaderLogo.js
+++ website/src/component/Header/HeaderLogo/HeaderLogo.js
@@ -7,9 +7,9 @@ class HeaderLogo extends Component {
       <Navbar.Brand className="NavLogo">
         <Nav.Link exact href="/">
           <a>
-        <img src="https://img.icons8.com/pastel-glyph/64/000000/travel-signpost.png" />
-        Half Mile
-        </a>
+            <img src="https://img.icons8.com/pastel-glyph/64/000000/travel-signpost.png" />
+            Half Mile
+          </a>
         </Nav.Link>
       </Navbar.Brand>
     );
