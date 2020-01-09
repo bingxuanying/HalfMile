4ee63eed62f404adc8352c3a9f3d924ce68ef938
CharlesYWL <xlyin@ucdavis.edu>
2020-01-09 04:30:23 -0800

    merge weili

--- website/src/component/HomeContent/ContentCover.js
+++ website/src/component/HomeContent/ContentCover.js
@@ -13,14 +13,24 @@ import {
   DayPickerRangeController
 } from "react-dates";
 import Calendar from "../Calender/Calender";
-
+import { Button, IconButton } from "@material-ui/core";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline";
 import "./ContentCover.sass";
 
 class ContentCover extends Component {
   constructor() {
     super();
 
-    this.state = { show: true };
+    this.state = {
+      show: true,
+      guestOpen: false,
+      adultOverZero: true,
+      childOverZero: false,
+      infantOverZero: false
+    };
     this.toggleDiv = this.toggleDiv.bind(this);
     this.handleStart = this.handleStart.bind(this);
     this.onSuggestSelect = this.onSuggestSelect.bind(this);
@@ -51,6 +61,12 @@ class ContentCover extends Component {
   }
 
   render() {
+    var adultColor = "grey";
+    var childColor = "grey";
+    var infantColor = "grey";
+    if (this.state.adultOverZero) adultColor = AirbnbColor;
+    if (this.state.childOverZero) childColor = AirbnbColor;
+    if (this.state.infantOverZero) infantColor = AirbnbColor;
     return (
       <div className="content-cover">
         <img src={coverImg} className="cover-image" alt="Cover Image"></img>
@@ -87,6 +103,94 @@ class ContentCover extends Component {
 
             <div className="start-box-row">
               <div className="start-box-subtitle">NUMBER OF PEOPLE</div>
+              <div className="start-box-guest">
+                <Button
+                  className="start-box-guest-btn"
+                  onClick={this.toggleGuest}
+                >
+                  {/* It shows how many guests depands on REDUX state */}
+                  <span>1 guest</span>
+                  {this.state.guestOpen ? (
+                    <ExpandLessIcon />
+                  ) : (
+                    <ExpandMoreIcon />
+                  )}
+                </Button>
+                {this.state.guestOpen && (
+                  <div className="start-box-guest-editbox">
+                    <div className="start-box-guest-editbox-form">
+                      <div className="start-box-guest-editbox-adult">
+                        <div className="text">Adults</div>
+                        <IconButton
+                          className="p-1"
+                          disabled={!this.state.adultOverZero}
+                        >
+                          <RemoveCircleOutlineIcon
+                            style={{
+                              fontSize: 36,
+                              color: adultColor
+                            }}
+                          />
+                        </IconButton>
+                        <div className="number">1+</div>
+                        <IconButton className="p-1">
+                          <AddCircleOutlineIcon
+                            style={{ fontSize: 36, color: AirbnbColor }}
+                          />
+                        </IconButton>
+                      </div>
+                      <div className="start-box-guest-editbox-child">
+                        <div className="text">Child</div>
+                        <IconButton
+                          className="p-1"
+                          disabled={!this.state.childOverZero}
+                        >
+                          <RemoveCircleOutlineIcon
+                            style={{
+                              fontSize: 36,
+                              color: childColor
+                            }}
+                          />
+                        </IconButton>
+                        <div className="number">0+</div>
+                        <IconButton className="p-1">
+                          <AddCircleOutlineIcon
+                            style={{ fontSize: 36, color: AirbnbColor }}
+                          />
+                        </IconButton>
+                      </div>
+                      <div className="start-box-guest-editbox-infant">
+                        <div className="text">Infant</div>
+                        <IconButton
+                          className="p-1"
+                          disabled={!this.state.infantOverZero}
+                        >
+                          <RemoveCircleOutlineIcon
+                            style={{
+                              fontSize: 36,
+                              color: infantColor
+                            }}
+                          />
+                        </IconButton>
+                        <div className="number">0+</div>
+                        <IconButton className="p-1">
+                          <AddCircleOutlineIcon
+                            style={{ fontSize: 36, color: AirbnbColor }}
+                          />
+                        </IconButton>
+                      </div>
+                    </div>
+                    <div className="start-box-guest-editbox-option">
+                      <span className="clear" onClick={this.guestClear}>
+                        Clear
+                      </span>
+                      <span className="save" onClick={this.guestSave}>
+                        Save
+                      </span>
+                    </div>
+                  </div>
+                )}
+              </div>
             </div>
             <div className="start-box-bottom">
               <button type="submit" onClick={this.handleStart}>
@@ -98,6 +202,20 @@ class ContentCover extends Component {
       </div>
     );
   }
+  toggleGuest = () => {
+    if (this.state.guestOpen) {
+      this.setState({ guestOpen: false });
+    } else {
+      this.setState({ guestOpen: true });
+    }
+  };
+  guestClear = () => {
+    // console.log("clear");
+    // Clear the State
+  };
+  guestSave = () => {
+    this.toggleGuest();
+  };
 }
 
 const mapStateToProps = state => {
@@ -106,7 +224,7 @@ const mapStateToProps = state => {
     homeAddress: state.plan[0].home
   };
 };
-
+const AirbnbColor = "rgba(24,143,148,1)";
 const mapDispatchToProps = () => {
   return {
     updateHomeAdress: planActions.updateHomeAdress,
