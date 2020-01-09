b9b6951b0192f74b70bc69882fe9e87f50e3f398
CharlesYWL <xlyin@ucdavis.edu>
2020-01-09 04:09:51 -0800

    finish startpage-guest number thing

--- website/src/component/HomeContent/ContentCover.js
+++ website/src/component/HomeContent/ContentCover.js
@@ -4,16 +4,32 @@ import Geosuggest from "react-geosuggest";
 import { connect } from "react-redux";
 import * as planActions from "../../actions/planActions";
 import "./ContentCover.sass";
-import 'react-dates/initialize';
-import 'react-dates/lib/css/_datepicker.css';
-import { DateRangePicker, SingleDatePicker, DayPickerRangeController } from 'react-dates';
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
+import {
+  DateRangePicker,
+  SingleDatePicker,
+  DayPickerRangeController
+} from "react-dates";
+
 import Calendar from "../Calender/Calender";
+import { Button, IconButton } from "@material-ui/core";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
+import RemoveCircleOutlineIcon from "@material-ui/icons/RemoveCircleOutline";
 
 class ContentCover extends Component {
   constructor() {
     super();
-    this.state = {show: true}
-    this.toggleDiv = this.toggleDiv.bind(this)
+    this.state = {
+      show: true,
+      guestOpen: false,
+      adultOverZero: true,
+      childOverZero: false,
+      infantOverZero: false
+    };
+    this.toggleDiv = this.toggleDiv.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.onSuggestSelect = this.onSuggestSelect.bind(this);
   }
@@ -41,11 +57,17 @@ class ContentCover extends Component {
   }
 
   toggleDiv = () => {
-    const {show} = this.state;
-    this.setState({show : !show})
-  }
+    const { show } = this.state;
+    this.setState({ show: !show });
+  };
 
   render() {
+    var adultColor = "grey";
+    var childColor = "grey";
+    var infantColor = "grey";
+    if (this.state.adultOverZero) adultColor = AirbnbColor;
+    if (this.state.childOverZero) childColor = AirbnbColor;
+    if (this.state.infantOverZero) infantColor = AirbnbColor;
     return (
       <div className="content-cover">
         <img src={coverImg} className="cover-image" alt="Cover Image"></img>
@@ -72,20 +94,110 @@ class ContentCover extends Component {
               <div className="start-box-row">
                 <div className="start-box-50subrow">
                   <div className="start-box-subtitle">START DATE</div>
-                  
-                  
                 </div>
                 <div className="start-box-50subrow">
                   <div className="start-box-subtitle">END DATE</div>
                 </div>
               </div>
               <tr>
-                <th><Calendar /></th>
+                <th>
+                  <Calendar />
+                </th>
                 <th></th>
-                <th><Calendar /></th>
+                <th>
+                  <Calendar />
+                </th>
               </tr>
               <div className="start-box-row">
                 <div className="start-box-subtitle">GUEST</div>
+                <div className="start-box-guest">
+                  <Button
+                    className="start-box-guest-btn"
+                    onClick={this.toggleGuest}
+                  >
+                    {/* It shows how many guests depands on REDUX state */}
+                    <span>1 guest</span>
+                    {this.state.guestOpen ? (
+                      <ExpandLessIcon />
+                    ) : (
+                      <ExpandMoreIcon />
+                    )}
+                  </Button>
+                  {this.state.guestOpen && (
+                    <div className="start-box-guest-editbox">
+                      <div className="start-box-guest-editbox-form">
+                        <div className="start-box-guest-editbox-adult">
+                          <div className="text">Adults</div>
+                          <IconButton
+                            className="p-1"
+                            disabled={!this.state.adultOverZero}
+                          >
+                            <RemoveCircleOutlineIcon
+                              style={{
+                                fontSize: 36,
+                                color: adultColor
+                              }}
+                            />
+                          </IconButton>
+                          <div className="number">1+</div>
+                          <IconButton className="p-1">
+                            <AddCircleOutlineIcon
+                              style={{ fontSize: 36, color: AirbnbColor }}
+                            />
+                          </IconButton>
+                        </div>
+                        <div className="start-box-guest-editbox-child">
+                          <div className="text">Child</div>
+                          <IconButton
+                            className="p-1"
+                            disabled={!this.state.childOverZero}
+                          >
+                            <RemoveCircleOutlineIcon
+                              style={{
+                                fontSize: 36,
+                                color: childColor
+                              }}
+                            />
+                          </IconButton>
+                          <div className="number">0+</div>
+                          <IconButton className="p-1">
+                            <AddCircleOutlineIcon
+                              style={{ fontSize: 36, color: AirbnbColor }}
+                            />
+                          </IconButton>
+                        </div>
+                        <div className="start-box-guest-editbox-infant">
+                          <div className="text">Infant</div>
+                          <IconButton
+                            className="p-1"
+                            disabled={!this.state.infantOverZero}
+                          >
+                            <RemoveCircleOutlineIcon
+                              style={{
+                                fontSize: 36,
+                                color: infantColor
+                              }}
+                            />
+                          </IconButton>
+                          <div className="number">0+</div>
+                          <IconButton className="p-1">
+                            <AddCircleOutlineIcon
+                              style={{ fontSize: 36, color: AirbnbColor }}
+                            />
+                          </IconButton>
+                        </div>
+                      </div>
+                      <div className="start-box-guest-editbox-option">
+                        <span className="clear" onClick={this.guestClear}>
+                          Clear
+                        </span>
+                        <span className="save" onClick={this.guestSave}>
+                          Save
+                        </span>
+                      </div>
+                    </div>
+                  )}
+                </div>
               </div>
               <div className="start-box-bottom">
                 <button type="submit">
@@ -98,6 +210,20 @@ class ContentCover extends Component {
       </div>
     );
   }
+  toggleGuest = () => {
+    if (this.state.guestOpen) {
+      this.setState({ guestOpen: false });
+    } else {
+      this.setState({ guestOpen: true });
+    }
+  };
+  guestClear = () => {
+    // console.log("clear");
+    // Clear the State
+  };
+  guestSave = () => {
+    this.toggleGuest();
+  };
 }
 
 const mapStateToProps = state => {
@@ -106,7 +232,7 @@ const mapStateToProps = state => {
     homeAddress: state.plan[0].home
   };
 };
-
+const AirbnbColor = "rgba(24,143,148,1)";
 const mapDispatchToProps = () => {
   return {
     updateHomeAdress: planActions.updateHomeAdress
--- website/src/component/Map/Map.jsx
+++ website/src/component/Map/Map.jsx
@@ -6,7 +6,7 @@ const MyMapComponent = withGoogleMap(props => (
   <GoogleMap
     // 5: state names; 8: city names; 10: one city name;
     // 13: city name and vague st; 15: city street view
-    defaultZoom={8}
+    defaultZoom={11}
     defaultCenter={{ lat: 34.0522342, lng: -118.2436849 }}
   >
     <Marker position={{ lat: 34.0522342, lng: -118.2436849 }} />
