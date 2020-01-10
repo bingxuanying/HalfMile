945cf0e8d1b139bf47351814032b69717780e9b6
CharlesYWL <34050457+CharlesYWL@users.noreply.github.com>
2020-01-09 21:50:00 -0800

    add snackbar

---
+++ website/src/component/SnackBar/SnackBar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./index.sass";
+
+class SnackBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+}
+
+
+export default SnackBar;
