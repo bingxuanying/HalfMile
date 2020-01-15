f73910a0a5021ba8c0a769e17df376f01590add9
Jerryhe <lolhe@ucdavis.edu>
2020-01-14 20:41:58 -0800

    delete useless css

--- website/src/component/Header/HeaderNav/HeaderNav.js
+++ website/src/component/Header/HeaderNav/HeaderNav.js
@@ -35,8 +35,8 @@ class HeaderNav extends Component {
 
     return (
       <Nav>
-        {/* <NavLog /> */}
-        <NavSign />
+        <NavLog />
+        {/* <NavSign /> */}
       </Nav>
       
     );
