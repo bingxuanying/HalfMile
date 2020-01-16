c3349542a1d104d72d46d5a30653b38d645e310e
CharlesYWL <xlyin@ucdavis.edu>
2020-01-16 02:22:26 -0800

    modify Calender

--- website/src/component/Calender/Calender.jsx
+++ website/src/component/Calender/Calender.jsx
@@ -7,27 +7,33 @@ import {
   SingleDatePicker,
   DayPickerRangeController
 } from "react-dates";
+import { Button } from "@material-ui/core";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import "./index.sass";
 
 class Calendar extends Component {
   constructor(props) {
     super(props);
     this.state = {
       startDate: null,
-      endDate: null
+      endDate: null,
+      pickerOpen: false
     };
   }
   render() {
     return (
-      <div className="App" style={{ position: "relative", zIndex: 100 }}>
-        <DateRangePicker 
-        startDate={this.state.startDate}
-        startDateId="your_unique_start_date_id"
-        endDate={this.state.endDate}
-        endDateId="you_unique_end_date_id"
-        onDatesChange={({startDate, endDate}) => this.setState({startDate, endDate})}
-        focusedInput={this.state.focusedInput}
-        onFocusChange={focusedInput => this.setState({focusedInput})}
-      />
+      <div className="app">
+        {/* <DateRangePicker
+          startDate={this.state.startDate}
+          startDateId="your_unique_start_date_id"
+          endDate={this.state.endDate}
+          endDateId="you_unique_end_date_id"
+          onDatesChange={({ startDate, endDate }) =>
+            this.setState({ startDate, endDate })
+          }
+          focusedInput={this.state.focusedInput}
+          onFocusChange={focusedInput => this.setState({ focusedInput })}
+        /> */}
         {/* <SingleDatePicker
           placeholder="mm/dd/yyy"
           date={this.state.date}
@@ -36,9 +42,31 @@ class Calendar extends Component {
           onFocusChange={({ focused }) => this.setState({ focused })}
           id="single-date"
         /> */}
+
+        {/** Below is for DND datePick use only */}
+        <ClickAwayListener onClickAway={this.toggleDayPicker}>
+          <div>
+            <Button onClick={this.toggleDayPicker}>Toggle DayPicker</Button>
+            {this.state.pickerOpen && (
+              <DayPickerRangeController
+                className="day-picker"
+                autoFocusEndDate={true}
+                initialStartDate={this.state.startDate}
+                endDate={this.state.endDate}
+                endDateId="you_unique_end_date_id"
+                onDatesChange={({ startDate, endDate }) =>
+                  this.setState({ startDate, endDate })
+                }
+              />
+            )}
+          </div>
+        </ClickAwayListener>
       </div>
     );
   }
+  toggleDayPicker = () => {
+    this.setState({ pickerOpen: !this.state.pickerOpen });
+  };
 }
 
 export default Calendar;
