fef1fb53be96ab23a289b15d3cfd8a04d7758a2b
Jerryhe <lolhe@ucdavis.edu>
2020-01-16 02:30:31 -0800

    updated sign up sass

--- website/src/component/Map/Map.jsx
+++ website/src/component/Map/Map.jsx
@@ -30,7 +30,7 @@ class Map extends Component {
         defaultCenter={{ lat: 34.0522342, lng: -118.2436849 }}
       >
         {/* home address */}
-        <Marker id={0} key={0} position={this.props.home.location} icon={{url: "https://img.icons8.com/dusk/100/000000/order-delivered.png"}}/>
+        <Marker id={0} key={0} position={this.props.home.location} icon={{url: "https://img.icons8.com/dusk/50/000000/order-delivered.png"}}/>
 
         {this.props.cities.map((city, idx) => {
           path.push(city.location);
@@ -42,7 +42,7 @@ class Map extends Component {
                 id={city.id}
                 key={city.id}
                 position={{ lat: city.location.lat, lng: city.location.lng }}
-                icon={{url: "https://img.icons8.com/bubbles/100/000000/building.png"}}
+                icon={{url: "https://img.icons8.com/bubbles/50/000000/building.png"}}
               />
               {/* lightgray - darkgray: interval = floor((hexdec_dark - hexdec_light) / this.props.cities.length) */}
               {/* color = "#" + stringfy(path.length * interval + lightgray) */}
